5a314f0916957e25f45215523a4316d588d4206c
Pierre-Louis Stenger <Pierrelouis.stenger@gmail.com>
2026-07-23 09:25:36 +0200

    Update 08_bracken_create_db.sh

--- 00_scripts/08_bracken_create_db.sh
+++ 00_scripts/08_bracken_create_db.sh
@@ -14,6 +14,14 @@ module load conda/4.12.0
 source ~/.bashrc
 conda activate metagenomics
 
+mkdir -p core_nt_k29/taxonomy
+mkdir -p core_nt_k29/library/added/
+
+#k2 download-taxonomy --db kraken_build --skip-maps
+
+#cd core_nt_k29/library/added/
+# Attention chemin sur Migale
+##ln -s /db/core_nt/core_nt_2026-04-04/fasta/core_nt.fsa file.fna
 
 # k29
 bracken-build \
@@ -22,9 +30,9 @@ bracken-build \
   -l 50 \
   -t 16
 
-# k25
-bracken-build \
-  -d /storage/groups/gdec/shared/Kraken_database/core_nt_k25 \
-  -k 25 \
-  -l 50 \
-  -t 16
+## k25
+#bracken-build \
+#  -d /storage/groups/gdec/shared/Kraken_database/core_nt_k25 \
+#  -k 25 \
+#  -l 50 \
+#  -t 16
